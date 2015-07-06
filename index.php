e3a0cc660a968c018c9cd0a5505685e0bd7db27d
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-07-06 19:36:17 +0200

    started to implement the cultusrego default theme

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $styleguide = new cultusrego(array(
   'source' => '../css/avalanche.css',
   'title' => '',
   'description' => 'a modular SASS framework',
-  'template_folder' => __DIR__ . '/theme/template',
+  'template_folder' => __DIR__ . '/theme/vendor/cultusrego_theme_default/template',
   'twig_cache' => FALSE,
 ));
 $styleguide->render();
