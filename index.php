7efecc169d0c425e63309dd781cb22ae8fe58657
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-07-23 19:45:52 +0200

    added a separate template folder and a separate header template and inlcuded the avalanche logo as inline svg

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $styleguide = new cultusrego(array(
   'title' => '',
   'description' => 'a modular SASS framework',
   'base_path' => 'theme/',
-  'template_folder' => __DIR__ . '/theme/vendor/cultusrego_theme_default/template',
+  'template_folder' => array(__DIR__ . '/theme/template', __DIR__ . '/theme/vendor/cultusrego_theme_default/template'),
   'twig_cache' => FALSE,
 ));
 $styleguide->render();
